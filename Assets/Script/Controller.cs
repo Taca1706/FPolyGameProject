b81999c41eb39a84a9c512fcecb888b99fe0aa5b
Taca1706 <117700726+Taca1706@users.noreply.github.com>
2024-03-08 03:24:55 +0700

    update_player

--- Assets/Script/Controller.cs
+++ Assets/Script/Controller.cs
@@ -4,34 +4,220 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
-    private Rigidbody2D rigidbody2d;
+    private Rigidbody2D rb;
     private SpriteRenderer spriteRenderer;
     private Animator animator;
+
+    //Thông s?
+    public float Spd = 6f;
+    public float DashForce = 30f;
+    
+    public float jumpForce = 10f;
+
+    public float currentHP;
+    public float maxHP;
+
+    //tr?ng l?c
+    public Vector2 newgravity;
+    private bool isgravitychange = false;
+
+    //anim & move
+    private bool isFacingRight = true;
+    float moveH;
+    float moveV;
+    bool isjump = true;
+    bool isclimb90 = false;
+
+    //v? trí chu?t
+    //private Vector3 targetPosition;
+
+
+    //??n
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 50f;
+
+    
+
+
+
+
+    float movePrefix = 6;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody2d = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    float movePrefix = 6;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //??i tr?ng l?c
+        if(collision.gameObject.CompareTag("Wall180"))
+        {
+            if(rb !=null)
+            {
+                rb.gravityScale = -2;
+                rb.AddForce(newgravity.normalized * rb.mass * Physics2D.gravity.magnitude, ForceMode2D.Force);
+                isgravitychange = true;
+            }
+        }
+        if (collision.gameObject.CompareTag("nonWall"))
+        {
+
+            if (rb != null)
+            {
+                rb.gravityScale = 1;
+                rb.AddForce(newgravity.normalized * rb.mass * Physics2D.gravity.magnitude, ForceMode2D.Force);
+                isgravitychange = false;
+            }
+        }
+
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            if(rb != null)
+            {
+                isjump = true;
+                isclimb90 = false;
+            }
+        } 
+        
+        if(collision.gameObject.CompareTag("Vground"))
+        {
+            if(rb != null)
+            {
+                isclimb90 = true;
+            }
+        }
+       
+
+
+
+    }
+
+
+
+
+
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        moveH = Input.GetAxis("Horizontal");
+        moveV = Input.GetAxis("Vertical");
+
+        if(!isclimb90)
         {
-            rigidbody2d.AddForce(Vector2.up * movePrefix, ForceMode2D.Impulse);
-        } 
-        if (Input.GetKeyDown(KeyCode.A))
+            rb.velocity = new Vector2(moveH * Spd, rb.velocity.y);
+            flip();
+            animator.SetFloat("Speed", Mathf.Abs(moveH));
+        }
+        
+        if(isclimb90)
         {
-            spriteRenderer.flipX = true;
-            rigidbody2d.AddForce(Vector2.left * movePrefix * 0.5f, ForceMode2D.Impulse);
+            rb.velocity = new Vector2(moveV * Spd, rb.velocity.x);
+            animator.SetBool("Isclimb90",true);
+        }
+
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            isclimb90 = false;
+            animator.SetBool("Isclimb90", false);
+        }
+        
+
+        
+
+
+        /*if (Input.GetKeyDown(KeyCode.A))
+        {
+            //spriteRenderer.flipX = true;
+            rb.AddForce(Vector2.left * movePrefix * 0.5f, ForceMode2D.Impulse);
         }
         if (Input.GetKeyDown(KeyCode.D))
         {
-            spriteRenderer.flipX = false;
-            rigidbody2d.AddForce(Vector2.right * movePrefix * 0.5f, ForceMode2D.Impulse);
+            //spriteRenderer.flipX = false;
+            rb.AddForce(Vector2.right * movePrefix * 0.5f, ForceMode2D.Impulse);
+
+
+        if (isgrachange == true)
+        {
+            animator.SetTrigger("Climb180");
         }
+        }*/
+
+        if (Input.GetKeyDown(KeyCode.Space) && isjump)
+        {
+            animator.SetTrigger("Jump");
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            isjump = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            animator.SetTrigger("RangedAttack");
+            Shoot();
+
+        } 
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            animator.SetTrigger("MeleeAttack");
+
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {   
+            /*targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            targetPosition.x = transform.position.x;
+            //targetPosition.y = transform.position.y; // Gi? nguyên v? trí z c?a nhân v?t
+
+            // Tính toán h??ng t? nhân v?t t?i v? trí chu?t
+            Vector3 direction = targetPosition - transform.position;
+
+            // Di chuy?n nhân v?t theo h??ng chu?t v?i t?c ?? c? ??nh trong 1 giây
+            transform.position += direction.normalized * DashForce * Time.deltaTime;*/
+            
+
+
+            //rb.AddForce(new Vector2(DashForce, 0f), ForceMode2D.Impulse);
+            animator.SetTrigger("Dash");
+        }
+
+       
+
+    }
+
+
+    private void flip()
+    {
+        if (isFacingRight && moveH < 0 || !isFacingRight && moveH > 0)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 sizeOfPlayer = transform.localScale;
+            sizeOfPlayer.x *= -1;
+            transform.localScale = sizeOfPlayer;
+        }
+    }
+
+
+    void Shoot()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        mousePosition.z = 0f;
+
+
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+
+        Vector2 direction = (mousePosition - transform.position).normalized;
+
+        rb.velocity = direction * bulletSpeed;
     }
 }
--- Assets/Script/PlayerDash/ArrowRotate.cs
+++ Assets/Script/PlayerDash/ArrowRotate.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class ArrowRotate : MonoBehaviour
 {
+    private GameObject Player;
+    private void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
     // Update is called once per frame
     void Update()
     {
+        transform.position = Player.transform.position;
         Vector3 direction = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
